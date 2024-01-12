18f2f4bc1a1487e5ec676ec8557b9baab8b4caf2
Cesar <67177783+gealtach@users.noreply.github.com>
2024-01-12 18:44:14 -0300

    Update route.ts

--- app/api/purchases/route.ts
+++ app/api/purchases/route.ts
@@ -3,7 +3,7 @@ import { getPurchasesByEmail } from "../controllers/purchasesController";
 
 export async function GET(req: Request) {
   try {
-    const { searchParams } = new URL(request.url);
+    const { searchParams } = new URL(req.url);
     const email = searchParams.get("email");
     const purchases = await getPurchasesByEmail(email as string);
     return NextResponse.json(purchases);
