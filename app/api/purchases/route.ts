c602a1365cf23af4aa6a41f9204f367206b6626f
Cesar <67177783+gealtach@users.noreply.github.com>
2024-01-12 18:49:40 -0300

    Update route.ts

--- app/api/purchases/route.ts
+++ app/api/purchases/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { getPurchasesByEmail } from "../controllers/purchasesController";
-
+export const dynamic = "force-dynamic";
 export async function GET(req: Request) {
   try {
     const { searchParams } = new URL(req.url);
