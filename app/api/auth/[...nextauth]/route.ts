5373eae25cc4480c3e6af7e31e6616b825634f16
Cesar <67177783+gealtach@users.noreply.github.com>
2024-01-13 18:40:49 -0300

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,8 +4,10 @@ import GoogleProvider from 'next-auth/providers/google';
 const handler = NextAuth({
     providers: [
         GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+            //clientId: process.env.GOOGLE_CLIENT_ID as string,
+           // clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+            clientId: '884443371716-h91ch70iehtmg2sj053po3km2mi1ts7f.apps.googleusercontent.com',
+            clientSecret: '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'
         })
     ],
 });
