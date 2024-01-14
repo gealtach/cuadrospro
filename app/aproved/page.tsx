8aba2da665e3a4f34de3643990a93b9ed900c216
gealtach <cleivaj93@gmail.com>
2024-01-13 21:22:07 -0300

    asdasd

--- app/aproved/page.tsx
+++ app/aproved/page.tsx
@@ -6,48 +6,47 @@ const Aproved = () => {
     const loadCart = () => {
         try {
             const cart = localStorage.getItem('cart');
-            console.log(cart);
-            
-            return cart;
+            return cart ? JSON.parse(cart) : null;
         } catch (error) {
             console.error('Error parsing JSON:', error);
             return null;
         }
     }
     
-    // const createPurchase = async (cart:any) => {
-    //     console.log(cart);
+    
+    const createPurchase = async (cart:any) => {
+        console.log(cart);
         
-    //     try {
-    //         const response = await fetch(`/api/purchase`, {
-    //             method: "POST",
-    //             headers:{
-    //                 'Content-Type': 'application/json',
-    //                 'Access-Control-Allow-Origin': '*',
-    //                 'Access-Control-Allow-Methods': 'POST',
-    //                 'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-    //             },
-    //             body: JSON.stringify(cart),
-    //         });
-    //         const data = await response.json();
-    //         console.log(data, 'im the purchase created bitches');
+        try {
+            const response = await fetch(`/api/purchase`, {
+                method: "POST",
+                headers:{
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'POST',
+                    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+                },
+                body: JSON.stringify(cart),
+            });
+            const data = await response.json();
+            console.log(data, 'im the purchase created bitches');
             
 
-    //     } catch (error) {
-    //         console.error('Error al realizar la solicitud:', error);
-    //     }
-    // }
+        } catch (error) {
+            console.error('Error al realizar la solicitud:', error);
+        }
+    }
 
     useEffect(()=> {
         const cart = loadCart();
         if(cart){
-            //createPurchase(cart);
+            createPurchase(cart);
         }
 
       },[])
   return (
     <div className='flex justify-center items-center w-screen h-screen'>
-        <div className='border bg-slate-100 p-4 flex flex-col justify-center items-center rounded'>
+        <div className='border bg-slate-100 p-4 flex flex-col gap-y-4 justify-center items-center rounded'>
             <IoCheckmarkDoneCircleOutline className='text-green-600' size={70} />
             <h1 className='text-3xl'>Tu compra se ha realizado con exito!!</h1>
         </div>
