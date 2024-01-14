bb7df5787c3c685e928e2cbbbe1a29f7d1acaa34
gealtach <cleivaj93@gmail.com>
2024-01-13 21:05:32 -0300

    as

--- app/aproved/page.tsx
+++ app/aproved/page.tsx
@@ -1,44 +1,42 @@
 'use client'
 import React, { useEffect } from 'react'
-import { useFileContext } from '../FileContext';
 import { IoCheckmarkDoneCircleOutline } from "react-icons/io5";
 
 const Aproved = () => {
-    const { dispatch } = useFileContext();
-    const loadCart = () => {
-        const cart = localStorage.getItem('cart');
-        return cart ? JSON.parse(cart) : null;
-    }
-    const createPurchase = async (cart:any) => {
-        console.log(cart);
+    // const loadCart = () => {
+    //     const cart = localStorage.getItem('cart');
+    //     return cart ? JSON.parse(cart) : null;
+    // }
+    // const createPurchase = async (cart:any) => {
+    //     console.log(cart);
         
-        try {
-            const response = await fetch(`/api/purchase`, {
-                method: "POST",
-                headers:{
-                    'Content-Type': 'application/json',
-                    'Access-Control-Allow-Origin': '*',
-                    'Access-Control-Allow-Methods': 'POST',
-                    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-                },
-                body: JSON.stringify(cart),
-            });
-            const data = await response.json();
-            console.log(data, 'im the purchase created bitches');
+    //     try {
+    //         const response = await fetch(`/api/purchase`, {
+    //             method: "POST",
+    //             headers:{
+    //                 'Content-Type': 'application/json',
+    //                 'Access-Control-Allow-Origin': '*',
+    //                 'Access-Control-Allow-Methods': 'POST',
+    //                 'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    //             },
+    //             body: JSON.stringify(cart),
+    //         });
+    //         const data = await response.json();
+    //         console.log(data, 'im the purchase created bitches');
             
 
-        } catch (error) {
-            console.error('Error al realizar la solicitud:', error);
-        }
-    }
+    //     } catch (error) {
+    //         console.error('Error al realizar la solicitud:', error);
+    //     }
+    // }
 
-    useEffect(()=> {
-        const cart = loadCart();
-        if(cart){
-            createPurchase(cart);
-        }
+    // useEffect(()=> {
+    //     const cart = loadCart();
+    //     if(cart){
+    //         createPurchase(cart);
+    //     }
 
-      },[dispatch])
+    //   },[])
   return (
     <div className='flex justify-center items-center w-screen h-screen'>
         <div className='border bg-slate-100'>
